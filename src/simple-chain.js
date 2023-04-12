2426484c1171f891682974483d39330c7417dc1e
Nervniy <c.zwerew2012@gmail.com>
2023-04-12 22:26:31 +0300

    done chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value){
+    if(value === undefined){value = ``}
+    this.values==undefined?this.values=[value]:this.values.push(value);
+    return this;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   finishChain(){
+    result = this.values.reduce((acc, item, index)=>index==0?acc+`( ${item} )`: acc+`~~( ${item} )`, '')
+    this.values = [];
+    return result;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(value){
+    if(value<=0 || value >this.values.length || typeof value != 'number'){
+      this.values = [];
+      throw new Error('You can\'t remove incorrect link!')
+    }
+    else{
+      this.values = this.values.filter((item)=>item!=this.values[value-1]); 
+      return this; 
+    }
   },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  reverseChain(){
+    this.values != undefined? this.values.reverse(): this.values = [];
+    return this;
   },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getLength(){
+    return this.values.length
   }
 };
 
