4cb843a000744d3871a8d246350e17bcf10f7d4a
Nervniy <c.zwerew2012@gmail.com>
2023-04-14 20:18:47 +0300

    done delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let nums = [];
+  let operatingNums = [...`${n}`]
+  for(let i = 0; i<`${n}`.length; i++){
+    nums.push(+(operatingNums.map((c, index)=>index!=i?c:'').join('')))
+  }
+  return Math.max(...nums)
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, a = 1){
+    let count = a;
+    for(let item of arr){
+      if(Array.isArray(item)){
+        count++;
+        this.calculateDepth(item, count);
+      }
+    }
+    return count;
   }
 }
 
